ccbd590677640190b343f67050a1aa0a88f03c45
samuel <samucrack2003@gmail.com>
2024-03-12 15:15:40 +0100

    si

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const multer = require('multer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configurar la carpeta de vistas
+
 app.set('views', path.join(__dirname, 'vistas'));
 app.set('view engine', 'ejs');  
 
-// Middleware para manejar datos de formularios
+
 const upload = multer();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
-// Ruta para manejar la solicitud GET a la raíz del servidor
 app.get('/', (req, res) => {
     res.render('vista');
 });
 
-// Ruta para manejar la solicitud POST de conversión de imagen
+
 app.post('/convert', upload.single('photo'), async (req, res) => {
     try {
-        // Convertir la imagen utilizando sharp
+        
         const outputBuffer = await sharp(req.file.buffer).toFormat('avif', {}).toBuffer();
 
-        // Enviar la imagen convertida al cliente para descarga
+        
         res.setHeader('Content-Type', 'image/avif');
         res.setHeader('Content-Disposition', 'attachment; filename="imagen_convertida.avif"');
         res.send(outputBuffer);
